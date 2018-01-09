e7a2f14136cb0fdc70475f7b5f83562a0491c81d
zachary schumpert <buphnezz@gmail.com>
2018-01-09 15:30:11 -0800

    finished 2nd question to about-me assignment

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 'use strict'
 
-var userName = prompt('Hi, what is your name?');
-console.log("The name of the user is: " + userName);
-alert('Great! Nice to meet you ' + userName + '.  I\'m Zachary.  Let\'s go on a fun adventure of getting to know just who I am....and stuff and junk =D!');
+// var userName = prompt('Hi, what is your name?');
+// console.log("The name of the user is: " + userName);
+// alert('Great! Nice to meet you ' + userName + '.  I\'m Zachary.  Let\'s go on a fun adventure of getting to know just who I am....and stuff and junk =D!');
 
-var pets = parseInt(prompt('How many pets do you think I have?'));
-
-// var howManyStates = prompt('How many states do you think I\'ve visited since I was born?');
+// var howManyStates = parseInt(prompt('How many states do you think I\'ve visited since I was born?'));
 // console.log("How many states the user thinks I've visited...tee hee!: " + howManyStates);
 
-if(howManyStates === 23) {
-    alert('What a guess!  You should seriously consider buying a lotto ticket with that kinda luck!');
-} else {
-    alert('Not quite, but great try!');
-}
+// if(howManyStates === 23) {
+//     alert('What a guess!  You should seriously consider buying a lotto ticket with that kinda luck!');
+// } else {
+//     alert('Not quite, but great try!');
+// }
