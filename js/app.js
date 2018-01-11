9e9e27f87065cdba4b0ed4ae3ee8623ffc120646
Beverly Pham <beverly.pham@gmail.com>
2018-01-11 14:41:13 -0800

    run fast function created

--- js/app.js
+++ js/app.js
@@ -8,27 +8,29 @@ var userName;
 //   alert('Great! Nice to meet you ' + userName + '.  I\'m Zachary.  Let\'s go on a fun adventure of getting to know just who I am....and stuff and junk =D!');
 // }
 
-function guessHowManyStates() {
-  var howManyStates = parseInt(prompt('How many states do you think I\'ve visited since I was born?'));
-  console.log('How many states the user thinks I\'ve visited...tee hee!: ' + howManyStates);
-  
-  if(howManyStates === 23) {
-    alert('What a guess!  You should seriously consider buying a lotto ticket with that kinda luck!');
+// function guessHowManyStates() {
+//   var howManyStates = parseInt(prompt('How many states do you think I\'ve visited since I was born?'));
+//   console.log('How many states the user thinks I\'ve visited...tee hee!: ' + howManyStates);
+
+//   if(howManyStates === 23) {
+//     alert('What a guess!  You should seriously consider buying a lotto ticket with that kinda luck!');
+//   } else {
+//     alert('Not quite, but great try!');
+//   }
+// }
+
+function guessCanIRunFast() {
+  var canIRunFast = prompt('Can I run faster than a cheetah?  (Please respond with either a YES or NO)').toUpperCase();
+  console.log('Did the user think I could run fast?: ' + canIRunFast);
+
+  if(canIRunFast === 'YES') {
+    alert('so....I was kind of lying, just a little.  There is no WAY I can run faster than a cheetah! However, you honor me with your answer and I will continue to train until I reach the speed of a cheetah!!');
+  } else if (canIRunFast === 'NO') {
+    alert('You are correct...I can only run AS FAST as a cheetah, not faster.');
   } else {
-    alert('Not quite, but great try!');
+    alert('Please enter YES or NO.');
   }
 }
-
-// var canIRunFast = prompt('Can I run faster than a cheetah?  (Please respond with either a YES or NO)').toUpperCase();
-// console.log('Did the user think I could run fast?: ' + canIRunFast);
-
-// if(canIRunFast === 'YES') {
-//   alert('so....I was kind of lying, just a little.  There is no WAY I can run faster than a cheetah! However, you honor me with your answer and I will continue to train until I reach the speed of a cheetah!!');
-// } else if (canIRunFast === 'NO') {
-//   alert('You are correct...I can only run AS FAST as a cheetah, not faster.');
-// } else {
-//   alert('Please enter YES or NO.');
-// }
 
 // var likesToDance = prompt('Take a guess, do I like to dance? (Y/N)').toUpperCase();
 // console.log('Does the user think I like to dance?: ' + likesToDance);
@@ -138,4 +140,5 @@ function guessHowManyStates() {
 // }
 
 // inputUserName();
-guessHowManyStates();
+// guessHowManyStates();
+guessCanIRunFast();
