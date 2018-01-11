755dc0982a09954dcfbe050d026f07c9883dfacb
Beverly Pham <beverly.pham@gmail.com>
2018-01-11 14:37:21 -0800

    username function created

--- js/app.js
+++ js/app.js
@@ -1,132 +1,138 @@
 'use strict';
 
-var userName = prompt('Hi, what is your name?');
-console.log('The name of the user is: ' + userName);
-alert('Great! Nice to meet you ' + userName + '.  I\'m Zachary.  Let\'s go on a fun adventure of getting to know just who I am....and stuff and junk =D!');
+var userName;
 
-var howManyStates = parseInt(prompt('How many states do you think I\'ve visited since I was born?'));
-console.log('How many states the user thinks I\'ve visited...tee hee!: ' + howManyStates);
-
-if(howManyStates === 23) {
-  alert('What a guess!  You should seriously consider buying a lotto ticket with that kinda luck!');
-} else {
-  alert('Not quite, but great try!');
+function inputUserName() {
+  userName = prompt('Hi, what is your name?');
+  console.log('The name of the user is: ' + userName);
+  alert('Great! Nice to meet you ' + userName + '.  I\'m Zachary.  Let\'s go on a fun adventure of getting to know just who I am....and stuff and junk =D!');
 }
 
-var canIRunFast = prompt('Can I run faster than a cheetah?  (Please respond with either a YES or NO)').toUpperCase();
-console.log('Did the user think I could run fast?: ' + canIRunFast);
+// var howManyStates = parseInt(prompt('How many states do you think I\'ve visited since I was born?'));
+// console.log('How many states the user thinks I\'ve visited...tee hee!: ' + howManyStates);
 
-if(canIRunFast === 'YES') {
-  alert('so....I was kind of lying, just a little.  There is no WAY I can run faster than a cheetah! However, you honor me with your answer and I will continue to train until I reach the speed of a cheetah!!');
-} else if (canIRunFast === 'NO') {
-  alert('You are correct...I can only run AS FAST as a cheetah, not faster.');
-} else {
-  alert('Please enter YES or NO.');
-}
+// if(howManyStates === 23) {
+//   alert('What a guess!  You should seriously consider buying a lotto ticket with that kinda luck!');
+// } else {
+//   alert('Not quite, but great try!');
+// }
 
-var likesToDance = prompt('Take a guess, do I like to dance? (Y/N)').toUpperCase();
-console.log('Does the user think I like to dance?: ' + likesToDance);
+// var canIRunFast = prompt('Can I run faster than a cheetah?  (Please respond with either a YES or NO)').toUpperCase();
+// console.log('Did the user think I could run fast?: ' + canIRunFast);
 
-if (likesToDance === 'Y') {
-  alert('Tricked you! It actually doesn\'t matter either way so let\'s dance right now!!');
-} else if (likesToDance === 'N') {
-  alert('Tricked you! It actually doesn\'t matter either way so let\'s dance right now!!');
-} else {
-  alert('Please enter Y or N.');
-}
+// if(canIRunFast === 'YES') {
+//   alert('so....I was kind of lying, just a little.  There is no WAY I can run faster than a cheetah! However, you honor me with your answer and I will continue to train until I reach the speed of a cheetah!!');
+// } else if (canIRunFast === 'NO') {
+//   alert('You are correct...I can only run AS FAST as a cheetah, not faster.');
+// } else {
+//   alert('Please enter YES or NO.');
+// }
 
-var likesKaraoke = prompt('How about Karaoke?  Do I like Karaoke? (Y/N)').toUpperCase();
-console.log('Does the user think I like Karaoke?: ' + likesKaraoke);
+// var likesToDance = prompt('Take a guess, do I like to dance? (Y/N)').toUpperCase();
+// console.log('Does the user think I like to dance?: ' + likesToDance);
 
-if (likesKaraoke === 'Y') {
+// if (likesToDance === 'Y') {
+//   alert('Tricked you! It actually doesn\'t matter either way so let\'s dance right now!!');
+// } else if (likesToDance === 'N') {
+//   alert('Tricked you! It actually doesn\'t matter either way so let\'s dance right now!!');
+// } else {
+//   alert('Please enter Y or N.');
+// }
 
-  var warmUp = prompt('You got that right! Can we warm up together then? (Y/N)').toUpperCase();
-  console.log('Did we warm up?: ' + warmUp);
+// var likesKaraoke = prompt('How about Karaoke?  Do I like Karaoke? (Y/N)').toUpperCase();
+// console.log('Does the user think I like Karaoke?: ' + likesKaraoke);
 
-  if (warmUp === 'Y') {
-    alert('Do-Re-Mi-Fa-So-La-Ti!');
-  } else if (warmUp === 'N') {
-    alert('Some singer you are!');
-  } else {
-    alert('Please input either Y or N');
-  }
+// if (likesKaraoke === 'Y') {
 
-} else if (likesKaraoke === 'N') {
-  alert('Actually, I LOVE karaoke!');
-} else {
-  alert('Please enter Y or N for your response.');
-}
+//   var warmUp = prompt('You got that right! Can we warm up together then? (Y/N)').toUpperCase();
+//   console.log('Did we warm up?: ' + warmUp);
 
-var catPerson = prompt('Am I a cat person? (Y or N)').toUpperCase();
-console.log('Does the user think I am a cat person?: ' + catPerson);
+//   if (warmUp === 'Y') {
+//     alert('Do-Re-Mi-Fa-So-La-Ti!');
+//   } else if (warmUp === 'N') {
+//     alert('Some singer you are!');
+//   } else {
+//     alert('Please input either Y or N');
+//   }
 
-if(catPerson === 'Y') {
-  alert('You got that right!  They\'re so purrrdy!');
-} else if(catPerson === 'N') {
-  alert('What\'s the meowtter with you?! Of course I love them kit-tays!');
-} else {
-  alert('The directions said to answer Y or N...get with the program brah!');
-}
+// } else if (likesKaraoke === 'N') {
+//   alert('Actually, I LOVE karaoke!');
+// } else {
+//   alert('Please enter Y or N for your response.');
+// }
 
-var dogPerson = prompt('Am I a dog person?(Y or N)').toUpperCase();
-console.log('Does the user think I am a dog person?: ' + dogPerson);
+// var catPerson = prompt('Am I a cat person? (Y or N)').toUpperCase();
+// console.log('Does the user think I am a cat person?: ' + catPerson);
 
-if (dogPerson === 'Y') {
-  alert('Hey, a person can be both OKAY!');
-} else if(dogPerson === 'N') {
-  alert('Doggos barking by day keeps a smile on my fay!...(translation - I love them boofin woofers!)');
-} else {
-  alert('Are you trolling me???  By now I figure you would have realized either input Y or N!...... :)');
-}
+// if(catPerson === 'Y') {
+//   alert('You got that right!  They\'re so purrrdy!');
+// } else if(catPerson === 'N') {
+//   alert('What\'s the meowtter with you?! Of course I love them kit-tays!');
+// } else {
+//   alert('The directions said to answer Y or N...get with the program brah!');
+// }
 
-var numberInMyHead = 77;
+// var dogPerson = prompt('Am I a dog person?(Y or N)').toUpperCase();
+// console.log('Does the user think I am a dog person?: ' + dogPerson);
 
-for (var c = 3; c > 0; c--) {
-  var numberGuessed = parseInt(prompt('What number am I thinking of? (Yes..I\'m being serious.  Take a guess but don\'t forget to input an actual number, no letters! =)'));
+// if (dogPerson === 'Y') {
+//   alert('Hey, a person can be both OKAY!');
+// } else if(dogPerson === 'N') {
+//   alert('Doggos barking by day keeps a smile on my fay!...(translation - I love them boofin woofers!)');
+// } else {
+//   alert('Are you trolling me???  By now I figure you would have realized either input Y or N!...... :)');
+// }
 
-  // numberGuessed = parseInt(numberGuessed);
+// var numberInMyHead = 77;
 
-  if (numberGuessed < numberInMyHead) {
+// for (var c = 3; c > 0; c--) {
+//   var numberGuessed = parseInt(prompt('What number am I thinking of? (Yes..I\'m being serious.  Take a guess but don\'t forget to input an actual number, no letters! =)'));
 
-    if (c === 0) {
-      alert('Aw, nice try. You are out of guesses.  Thanks for playing!');
-      console.log(c);
-    }
+//   // numberGuessed = parseInt(numberGuessed);
 
-    if (c === 1) {
-      alert('higher. ' + 'You have ' + c + ' guess remaining!');
-      console.log(c);
-    }
+//   if (numberGuessed < numberInMyHead) {
 
-    else {
-      alert('higher. ' + 'You have ' + c + ' guesses remaining!');
-      console.log(c);
-    }
+//     if (c === 0) {
+//       alert('Aw, nice try. You are out of guesses.  Thanks for playing!');
+//       console.log(c);
+//     }
 
-  } else if (numberGuessed > numberInMyHead) {
+//     if (c === 1) {
+//       alert('higher. ' + 'You have ' + c + ' guess remaining!');
+//       console.log(c);
+//     }
 
-    if (numberGuessed > numberInMyHead && c === 0) {
-      alert('Aw, nice try. You are out of guesses.  Thanks for playing!');
-      console.log(c);
-      break;
-    }
+//     else {
+//       alert('higher. ' + 'You have ' + c + ' guesses remaining!');
+//       console.log(c);
+//     }
 
-    alert('lower. ' + 'You have ' + c + ' guesses remaining!');
-    console.log(c);
-  }
+//   } else if (numberGuessed > numberInMyHead) {
 
-  if (numberGuessed > numberInMyHead && c === 1) {
-    alert('lower. ' + 'You have ' + c + ' guess remaining!');
-    continue;
+//     if (numberGuessed > numberInMyHead && c === 0) {
+//       alert('Aw, nice try. You are out of guesses.  Thanks for playing!');
+//       console.log(c);
+//       break;
+//     }
 
-  } else if (numberGuessed > numberInMyHead && c === 0) {
-    alert('Aw, nice try. You are out of guesses.  Thanks for playing!');
-    console.log(c);
-    break;
+//     alert('lower. ' + 'You have ' + c + ' guesses remaining!');
+//     console.log(c);
+//   }
 
-  } else {
-    alert('correct the number was ' + numberInMyHead);
-    console.log(c);
-    break;
-  }
-}
+//   if (numberGuessed > numberInMyHead && c === 1) {
+//     alert('lower. ' + 'You have ' + c + ' guess remaining!');
+//     continue;
+
+//   } else if (numberGuessed > numberInMyHead && c === 0) {
+//     alert('Aw, nice try. You are out of guesses.  Thanks for playing!');
+//     console.log(c);
+//     break;
+
+//   } else {
+//     alert('correct the number was ' + numberInMyHead);
+//     console.log(c);
+//     break;
+//   }
+// }
+
+inputUserName();
